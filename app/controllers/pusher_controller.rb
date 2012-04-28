add17341281ae97e0c6028f7bd81949f5b063559
Johnathan Croom <johnathancroom@gmail.com>
2012-04-28 16:36:41 -0700

    Fixed pusher#auth bug while not logged in

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -6,9 +6,9 @@ class PusherController < ApplicationController
 
   def auth
       response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
-        :user_id => @current_user.id || "",
+        :user_id => (@current_user.id if logged_in?) || 1000*rand(),
         :user_info => {
-          :name => @current_user.username
+          :name => (@current_user.username if logged_in?) || "Guest"
         }
       })
       render :json => response
