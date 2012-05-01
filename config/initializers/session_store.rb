cda7b8ce49dfdfae56a2e98e848062f30aff1e7e
Johnathan Croom <johnathancroom@gmail.com>
2012-04-30 21:37:50 -0700

    Persitent sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-BiddingFeeAuction::Application.config.session_store :cookie_store, key: '_auction_session'
+BiddingFeeAuction::Application.config.session_store :cookie_store, { key: '_auction_session', :expire_after => 30.days }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
