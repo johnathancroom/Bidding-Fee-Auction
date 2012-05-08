bcbd8696891817885371139eab9ef0bd1c575016
Johnathan Croom <johnathancroom@gmail.com>
2012-05-07 17:07:07 -0700

    Search animation improvements

--- app/assets/javascripts/design/auctions.js
+++ app/assets/javascripts/design/auctions.js
@@ -1,29 +1,35 @@
 /* Search bar show/hide */
 $("#search input[type=text]").on({
   mouseenter: function() {
-    $(this).stop().animate({
-      backgroundColor: "#FFF",
-      borderTopColor: "#D1D1D1",
-      borderRightColor: "#d1d1d1",
-      borderBottomColor: "#d1d1d1",
-      borderLeftColor: "#d1d1d1",
-      paddingLeft: 20,
-      right: 0,
-      width: 180
-    }, 500);
+    $(this).stop()
+      .animate({
+        width: 180
+      }, 100)
+      .animate({
+        backgroundColor: "#FFF",
+        borderTopColor: "#D1D1D1",
+        borderRightColor: "#d1d1d1",
+        borderBottomColor: "#d1d1d1",
+        borderLeftColor: "#d1d1d1",
+        paddingLeft: 20,
+        right: 0
+      }, 500);
   },
   mouseleave: function() {
     $(this).blur();
     
-    $(this).stop().animate({
-      backgroundColor: "transparent",
-      borderTopColor: "transparent",
-      borderRightColor: "transparent",
-      borderBottomColor: "transparent",
-      borderLeftColor: "transparent",
-      paddingLeft: 25,
-      right: "-6px",
-      width: 0
-    }, 500);
+    $(this).stop()
+      .animate({
+        width: 0
+      }, 100)
+      .animate({
+        backgroundColor: "transparent",
+        borderTopColor: "transparent",
+        borderRightColor: "transparent",
+        borderBottomColor: "transparent",
+        borderLeftColor: "transparent",
+        paddingLeft: 25,
+        right: "-6px"
+      }, 500);
   }
 });
